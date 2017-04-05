4e5d8238a6cddadbfd33e196d0caa7857c874032
xiongbeer <593423728@qq.com>
2017-04-05 21:31:13 +0800

    引入jsoup

--- src/main/java/com/xiongbeer/webveins/selector/Html.java
+++ src/main/java/com/xiongbeer/webveins/selector/Html.java
@@ -1,12 +1,24 @@
-package com.xiongbeer.webveins.utils;
+package com.xiongbeer.webveins.selector;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 
 public class Html {
-	private String entity;
+	private Document html;
+	
 	public Html(String entity){
-		this.entity = entity;
+		html = Jsoup.parse(entity);
 	}
 	
+	public Element body(){
+		return html.body();
+	}
+	
+	
 	public String toString(){
-		return entity;
+		return html.toString();
 	}
 }
