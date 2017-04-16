54a94ae717f5a777f192f2ddd5d86f8e1bdcf11f
xiongbeer <593423728@qq.com>
2017-04-16 20:22:35 +0800

    修复测试文件存放路径

--- src/main/java/com/xiongbeer/Configuration.java
+++ src/main/java/com/xiongbeer/Configuration.java
@@ -4,8 +4,8 @@ package com.xiongbeer;
  * Created by shaoxiong on 17-4-11.
  */
 public class Configuration {
-    public static final String BLOOM_SAVE_PATH    = "/media/shaoxiong/EmdeBoas/urlbloomvalue.dat";
-    public static final String C_BLOOM_SAVE_PATH  = "/media/shaoxiong/EmdeBoas/urlbloomvalue_compressed.dat";
+    public static final String BLOOM_SAVE_PATH    = "temp/urlbloomvalue.dat";
+    public static final String C_BLOOM_SAVE_PATH  = "temp/urlbloomvalue_compressed.dat";
     public static final int BLOOM_WRITE_BLOCK     = 1024*1024;
 
     public static final int CHECK_TIME            = 30;
