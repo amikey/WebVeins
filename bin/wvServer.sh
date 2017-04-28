e649832bbfbcd9a1ea6037363c364ef3e4089a56
xiongbeer <593423728@qq.com>
2017-04-28 22:20:35 +0800

    Server端的启动脚本

--- bin/wvServer.sh
+++ bin/wvServer.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-WEBVEINS_HOME=/home/shaoxiong/Code/Java/WebVeins/bin
 WEBVEINS_PATH=""
-for i in "$WEBVEINS_HOME"/../lib/*.jar
+for i in "$WEBVEINS_HOME"/lib/*.jar
 do
     WEBVEINS_PATH="$i:$WEBVEINS_PATH"
 done
+WEBVEINS_PATH=$WEBVEINS_HOME/webveins.jar:$WEBVEINS_PATH
+
+export WEBVEINS_CONF_PATH=$WEBVEINS_HOME/conf
+
+echo 'start server...'
+java -cp $WEBVEINS_PATH:webveins.jar com.xiongbeer.WebVeinsMain server
