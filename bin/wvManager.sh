2468f1f7478acf112e8ea5de3682edd4c02a0e68
xiongbeer <593423728@qq.com>
2017-04-29 23:22:37 +0800

    添加了Manager的启动脚本

---
+++ bin/wvManager.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+isrun=`jps | grep WebVeinsMain`
+if [ "$isrun" = "" ]
+then
+    echo 'start manager...'
+else
+    echo 'manager has already running'
+    exit 1
+fi
+
+WEBVEINS_PATH=""
+for i in "$WEBVEINS_HOME"/lib/*.jar
+do
+    WEBVEINS_PATH="$i:$WEBVEINS_PATH"
+done
+WEBVEINS_PATH=$WEBVEINS_HOME/webveins.jar:$WEBVEINS_PATH
+
+export WEBVEINS_CONF_PATH=$WEBVEINS_HOME/conf
+
+java -cp $WEBVEINS_PATH:webveins.jar com.xiongbeer.WebVeinsMain manager
